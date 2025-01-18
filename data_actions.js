b16029598cbefae042bd28cdea668fb65e4aa8a6
bradyvogt <bradyvogt5@gmail.com>
2025-01-18 15:23:04 -0600

    Added rating and cook time in recipe header

--- data_actions.js
+++ data_actions.js
@@ -1,11 +1,52 @@
 // Ingredient actions
 const formatIngredient = (ingredientObject) => {
     return ingredientObject
-        .map(str => (typeof str === "string" ? str.trim() : str)) // Trim each string
-        .filter(str => typeof str === "string" && str.length > 0) // Filter out empty strings
+        .map(ing => (typeof ing === "string" ? getProperCase(ing) : ing))
+        .filter(ing => typeof ing === "string" && ing.length > 0) // Filter out empty ingredients
         .join(" "); // Join with spaces
 }
 
 const getIngredientName = (ingredientObject) => {
     return ingredientObject[1].toLowerCase().trim();
 }
+
+const getTotalCookTime = (ingredientObject) => {
+    return ingredientObject.prep_time + ingredientObject.cook_time;
+}
+
+// Format actions
+const formatTitle = (title) => {
+    return getProperCase(title);
+}
+
+const formatTime = (minutes) => {
+
+    minutes = Math.floor(minutes);
+
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+
+    // Build formatted '2 hr 30 min'
+    return `${hours ? `${hours} hr` : ''} ${remainingMinutes ? `${remainingMinutes} min` : ''}`.trim();
+}
+
+const formatRating = (rating) => {
+    return rating && rating >= 0 ? `${Math.round(rating*2)/2}/10` : 'NR';
+}
+
+const getProperCase = (string) => {
+    return string
+        .toLowerCase()
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize first letter
+        .join(" ");
+}
+
+// Comparators (Sort Functions)
+var titleSortFunction = (a, b, isAsc) => {
+    return isAsc ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title);
+};
+
+var ratingSortFunction = (a, b, isAsc) => {
+    return isAsc ? a.rating - b.rating : b.rating - a.rating;
+};
