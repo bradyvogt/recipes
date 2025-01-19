1e4cc75931d5223a99c3c8cec665311799453f74
bradyvogt <bradyvogt5@gmail.com>
2025-01-19 09:37:40 -0600

    Added more meals

--- data.js
+++ data.js
@@ -55,5 +55,87 @@ const recipes = [
         "prep_time": 20,
         "cook_time": 45,
         "category": "dessert"
+    },
+    {
+        "title": "Beef Enchiladas",
+        "prep_time": 30,
+        "cook_time": 20,
+        "rating": 8,
+        "category": "main dish",
+        "ingredients": [
+            ["1 1/2 lbs", "ground beef"],
+            ["10", "tortillas"],
+            ["1 can", "enchilada sauce"],
+            ["10 oz", "tomato soup"],
+            ["10 oz", "cream of chicken"],
+            ["1 pkg", "taco seasoning"],
+            ["16 oz", "shredded cheddar"]
+        ]
+    },
+    {
+        "title": "Jamaican Jerk Chicken",
+        "prep_time": 40,
+        "cook_time": 20,
+        "servings": 6,
+        "category": "main dish",
+        "ingredients": [
+            ["1 lb", "chicken tenders"],
+            ["3 tbsp", "soy sauce"],
+            ["2", "green onions"],
+            ["1 tbsp", "apple cider vinegar"],
+            ["1 tbsp", "jamaican jerk rub"],
+            ["2 tbsp", "brown sugar"],
+            ["1 tbsp", "vegetable oil"]
+        ],
+        "instructions": [
+            "Heat oil in skillet over medium heat",
+            "Cook chicken & marinade until liquid evaporates"
+        ]
+    },
+    {
+        "title": "Apricot Chicken",
+        "prep_time": 20,
+        "cook_time": 20,
+        "category": "main dish",
+        "ingredients": [
+            ["1 head", "broccoli"],
+            ["1 lb", "chicken breast"],
+            ["1/2 tsp", "garlic powder"],
+            ["3 tbsp", "flour"],
+            ["3 tbsp", "olive oil"],
+            ["3/4 cup", "apricot jam"],
+            ["1 clove", "garlic"],
+            ["2 tbsp", "soy sauce"],
+            ["1 1/2 tbsp", "brown sugar"],
+            ["Serve with", "rice"]
+        ],
+        "instructions": [
+            "Preheat oven to 425"
+        ]
+    },
+    {
+        "title": "Quiche",
+        "prep_time": 40,
+        "cook_time": 20,
+        "category": "main dish",
+        "ingredients": [
+            ["1", "pie crust", "refrigerated"],
+            ["6", "eggs"],
+            ["3/4 cup", "milk"],
+            ["Some", "salt & pepper"],
+            ["1 cup", "cooked ham"],
+            ["1 1/2 cup", "shredded cheese"],
+            ["3 tbsp", "green onions"]
+        ],
+        "instructions": [
+            "Heat oil in skillet over medium heat",
+            "Cook chicken & marinade until liquid evaporates"
+        ]
+    },
+    {
+        "title": "Overnight Oats",
+        "prep_time": 10,
+        "cook_time": 300,
+        "category": "breakfast",
     }
 ];
